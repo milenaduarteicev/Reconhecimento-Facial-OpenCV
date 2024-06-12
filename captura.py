f709404233e7db9e115950a378b14dbb4274f640
milenaduarteicev <milena.duarte@somosicev.com>
2024-06-12 17:25:17 -0300

    Detecção de Face

--- captura.py
+++ captura.py
@@ -1,7 +1,21 @@
 import cv2
 
+classificador = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 camera = cv2.VideoCapture(0)
+amostra = 1
+
 
 while (True):
     conectado, imagem = camera.read()
+    imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY) #converter imagens em escalas de cinza
+    facesDetectadas = classificador.detectMultiScale(imagemCinza, scaleFactor=1.5, minSize=(100,100)) #
     
+    for(x, y, l, a) in facesDetectadas:
+        cv2.rectangle(imagem, (x, y), (x + l, y + a), (0, 0, 255, 2))
+    
+    cv2.imshow("Face", imagem)
+    cv2.waitKey(1)
+    
+camera.realese ()
+cv2.destroyAllWindows()
+
