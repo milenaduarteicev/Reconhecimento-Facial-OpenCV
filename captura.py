ea3fa4e139cac6066d46cdd8121bf504130b0e58
milenaduarteicev <milena.duarte@somosicev.com>
2024-06-19 20:27:10 -0300

    Capturando Olhos

--- captura.py
+++ captura.py
@@ -1,8 +1,8 @@
 import cv2
+import numpy as np
 
-haarcascade_path = 'haarcascade_frontalface_default.xml'
-
-classificador = cv2.CascadeClassifier(haarcascade_path)
+classificador = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+classificadorOlho = cv2.CascadeClassifier("haarcascade_eye.xml")
 camera = cv2.VideoCapture(0)
 amostra = 1 #controla quantas fotos ele vai tirar
 numeroAmostras = 25 #número de fotos que o algoritmo vai tirar por pessoa, diversas posições
@@ -13,17 +13,26 @@ print("Diga X!!!")
 while (True):
     conectado, imagem = camera.read()
     imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY) #converter imagens em escalas de cinza
-    facesDetectadas = classificador.detectMultiScale(imagemCinza, scaleFactor=1.5, minSize=(100,100)) #
+    print(np.average(imagemCinza))
+    '''facesDetectadas = classificador.detectMultiScale(imagemCinza, scaleFactor=1.5, minSize=(100,100)) #
     
     for(x, y, l, a) in facesDetectadas:
-        cv2.rectangle(imagem, (x, y), (x + l, y + a), (0, 0, 255, 2))
+        cv2.rectangle(imagem, (x, y), (x + l, y + a), (0, 0, 255), 2)
+        regiao = imagem[y:y + a, x:x + l]
+        regiaoCinzaOlho = cv2.cvtColor(regiao, cv2.COLOR_BGR2GRAY)
+        olhosDetectados = classificadorOlho.detectMultiScale(regiaoCinzaOlho)
+        for (ox, oy, ol, oa) in olhosDetectados:
+            cv2.rectangle(regiao, (ox, oy), (ox + ol, oy + oa), (0, 255, 0), 2)
+        
         if cv2.waitKey(1) & 0xFF == ord("q"): #salvar imagem
-            imagemFace = cv2.resize(imagemCinza[y:y + a, x:x + 1], (largura, altura))
-            cv2.imwrite("fotos/pessoa." + str(id) + "." + str(amostra) + ".jpg", imagemFace)
-            print("foto" + str(amostra)+ "capturada com sucesso")
+            if np.average(imagemCinza) > 110:
+                imagemFace = cv2.resize(imagemCinza[y:y + a, x:x + l], (largura, altura))
+                cv2.imwrite("fotos/id." + str(id) + "." + str(amostra) + ".jpg", imagemFace)
+                print("[foto" + str(amostra)+ "capturada com sucesso]")
+                amostra += 1 '''
             
     cv2.imshow("Face", imagem)
-    #cv2.waitKey(1)
+    cv2.waitKey(1)
     if(amostra >= numeroAmostras + 1):
         break
     
